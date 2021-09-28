81c4437816a89d959b43d3cc939ada79dc74e5a5
profe Joel <joel.torres@ulagos.cl>
2021-09-28 16:24:13 -0300

    Clase práctica 7 - martes 28/09

---
+++ CSHARP/Funcion1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Funcion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, f;
+            Console.WriteLine("Ingrese el valor de X");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            f = ((x+1)*(x+1)) + ((2*x)*(2*x)); // (x+1)^2 + (2x)^2, la operación de potencia no es permitida en C#
+
+            Console.WriteLine("El resultado es: " + f);
+        }
+    }
+}
---
+++ PYTHON/Funcion1/funcion1.py
@@ -0,0 +1,7 @@
+print("Ingrese el valor de X:")
+x = float(input())
+
+#f = ((x+1)*(x+1)) + ((2*x)*(2*x))
+f = (x+1)**2 + (2*x)**2 # el operador ** la potencia
+
+print("El resultado es: " + str(f)) # str() tranforma un número a una palabra
